3d8351170a39e71ae2fea5bf20abc04e9c7a9905
Kevin Alavik <kevin@alavik.se>
2023-04-05 06:57:15 +0000

    wip on the cli

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo "Thanks for using chrome pie, very much love from @pufferisadev"
 echo "--------------------------------------------------------------"
 gcc src/cmd/main.c && sudo chmod +x a.out && mv a.out ./pie
 echo "Successfully compiled pie, (./pie)"
-echo "pie will now do the futher installing: "
-./pie
+echo "Now check the install guide (https://github.com/KevinAlavik/chrome-pie/blob/main/INSTALL.md) just skip the Build and Install part. And you should be good to go"
+sudo mv ./pie /usr/bin
--- src/cmd/main.c
+++ src/cmd/main.c
@@ -1,10 +1,23 @@
-/*
- * Credits: Kevin Alavik
- */
-
 #include <stdio.h>
-
-int main() {
-  printf("Work in progress\n\n");
-  return 0;
-} 
+#include <stdlib.h>
+  
+int main(int argc, char* argv[]) {
+    if (argv[1] = "--help") {
+      printf("Chrome Pie\n--help - Displays this message\n\n");
+    } 
+    if (argv[1] = "run") {
+      if(argv[2] = "--help") {
+        printf("Run commands\n--help - Displays this message\n--stable - Installs the stable version of pie\n--canary - Innstalls the canary version of pie\n");
+        return 1;
+      } if (argv[2] = "--stable ") {
+        printf("Set deafult install/run option to **stable**");
+      } else {
+        printf("Chrome Pie\n--------------\nWork in progress\n---------------\n");
+      }
+      return 0;
+    } else {
+      printf("Did not find command check --help for a list of the commands");
+      return 0;
+    }
+    return 0;
+}
