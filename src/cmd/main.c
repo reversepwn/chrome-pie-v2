004fe7cfbefde6a686a45c742069a222278597f0
Kevin Alavik <kevin@alavik.se>
2023-04-06 06:12:11 +0000

    fix

--- src/cmd/main.c
+++ src/cmd/main.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
     return 1;
   } else if (strcmp(instr, "run") == 0) {
     if (argc == 2) {
-      printf("Chrome Pie\n--------------\nWork in progress\n---------------\n");
+      printf("Work in progress \n");
     } else if (argc == 3) {
       if (strcmp(argv[2], "--stable") == 0) {
         printf("Set default install/run option to " BWHT "stable\n");
@@ -28,10 +28,12 @@ int main(int argc, char **argv) {
       } else if (strcmp(argv[2], "--help") == 0) {
         printf("Run commands\n--help - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
       } else {
-        printf(RED "Did not find argument: " reset BWHT GRN "%s, " reset "see" BWHT " --help or -h" reset "for a list of arguments\n", argv[2]);
+        printf(RED "Did not find argument: " reset BWHT GRN "%s, " reset "see" BWHT " --help or -h " reset "for a list of arguments\n", argv[2]);
       }
     } else if (argc == 4 && strcmp(argv[2], "--help") == 0) {
-      printf("Run commands\n--help - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
+      printf("Run commands\n--help, -h - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
+    } else if (argc == 4 && strcmp(argv[2], "-h") == 0) {
+      printf("Run commands\n--help, -h - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
     } else {
       printf(RED "Invalid number of arguments, " reset "see " BWHT "--help or -h " reset "for a list of arguments\n");
     }
