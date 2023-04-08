7d2cf1b086576bce922664a6e29c5d0817efcca2
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-04-08 10:24:18 +0200

    Wip

--- src/main.c
+++ src/main.c
@@ -1,106 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "lib/colors.h"
 #include "lib/tweak-loader.h"
 #include "lib/tweak-handler.h"
 #include "lib/bypass.h"
 #include "lib/sideload.h"
 #include "lib/chromeos.h"
+#include "lib/update-handler.h"
 
-int main(int argc, char **argv)
+void error(int type);
+void help();
+
+int main(int argc, char *argv[])
 {
-  if (argc < 2)
-  {
-    printf(RED "Expected a command, " reset "see " BWHT "--help or -h " reset "for a list of commands\n");
-    return 1;
-  }
-
-  char *instr = argv[1];
-
-  if (strcmp(instr, "--help") == 0)
-  {
-    printf(RED "Chrome Pie\n" reset "--help, -h - Displays this message\nrun - runs pie\ntweak - create tweak command(s)\n");
-    return 1;
-  }
-  else if (strcmp(instr, "-h") == 0)
-  {
-    printf(RED "Chrome Pie\n" reset "--help, -h - Displays this message\nrun - runs pie\ntweak - create tweak command(s)\n");
-    return 1;
-  }
-  else if (strcmp(instr, "tweak") == 0)
-  {
-    printf(RED "Chrome Pie\n" reset "create - Creates an template file for developing tweaks\n");
-    if (strcmp(argv[2], "create") == 0)
+    if (argc == 1)
     {
-      if (strcmp(argv[3], "-n") == 0)
-      {
-        if (strcmp(argv[4], "") == 1)
-        {
-          tweakTemplate(argv[4]);
-        }
-        else
-        {
-          printf(RED "Expected an name, " reset "do \"tweak name\" after (replace tweak name with your tweaks name)\n");
-        }
-      }
-      else if (strcmp(argv[3], "-h") == 0)
-      {
-        printf(RED "Chrome Pie Tweak Builder\n" reset "-n - Set the name of the tweak\n");
-      }
-      else if (strcmp(argv[3], "--help") == 0)
-      {
-        printf(RED "Chrome Pie Tweak Builder\n" reset "-n - Set the name of the tweak\n");
-      }
-      printf(RED "Expected an argument, " reset "see " BWHT "--help or -h " reset "for a list of commands\n");
+        error(1);
     }
-  }
-  else if (strcmp(instr, "pico") == 0)
-  {
-    runPiePico();
-  }
-  else if (strcmp(instr, "run") == 0)
-  {
+
     if (argc == 2)
     {
-      runPie();
+        if (strcmp(argv[1], "--help") == 0)
+        {
+            help();
+        } else if(strcmp(argv[1], "-h") == 0) {
+            help();
+        } else if(strcmp(argv[1], "update") == 0) {
+            update();
+        } else {
+            error(2);
+        }
     }
-    else if (argc == 3)
-    {
-      if (strcmp(argv[2], "--stable") == 0)
-      {
-        printf("Set default install/run option to " BWHT "stable\n");
-      }
-      else if (strcmp(argv[2], "--canary") == 0)
-      {
-        printf("Set default install/run option to " BWHT "canary\n");
-      }
-      else if (strcmp(argv[2], "--help") == 0)
-      {
-        printf("Run commands\n--help - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
-      }
-      else
-      {
-        printf(RED "Did not find argument: " reset BWHT GRN "%s, " reset "see" BWHT " --help or -h " reset "for a list of arguments\n", argv[2]);
-      }
-    }
-    else if (argc == 4 && strcmp(argv[2], "--help") == 0)
-    {
-      printf("Run commands\n--help, -h - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
-    }
-    else if (argc == 4 && strcmp(argv[2], "-h") == 0)
-    {
-      printf("Run commands\n--help, -h - Displays this message\n--stable - Installs the stable version of pie\n--canary - Installs the canary version of pie\n");
-    }
-    else
-    {
-      printf(RED "Invalid number of arguments, " reset "see " BWHT "--help or -h " reset "for a list of arguments\n");
-    }
-  }
-  else
-  {
-    printf(RED "Invalid command: " reset BWHT GRN "%c, " reset "see " BWHT "--help or -h" reset "for a list of commands\n", *instr);
-  }
+}
 
-  return 0;
+void error(int type)
+{
+    if (type == 1)
+    {
+        printf(RED "Expected a parameter or a command, " reset GRN "see --help or -h for all the avalible commands and parameters\r\n");
+    }
+    if (type == 2)
+    {
+        printf(RED "Did not find command or parameter, " reset GRN "see --help or -h for all the avalible commands and parameters\r\n");
+    }
+}
+
+void help()
+{
+    printf(UYEL "Chrome Pie" reset "\n -help, -h - Displays this message\nrun - runs pie\ntweak - create tweak command(s)\r\n");
 }
