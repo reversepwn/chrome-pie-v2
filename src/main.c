9539d200921738dc381a6c2c16342718ce044d69
Kevin Alavik <kevin@alavik.se>
2023-04-26 08:34:06 +0000

    Added things

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include "lib/usb.h"
-
-void getAllUsb();
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf(RED "Work in progress, fakeos is work in progress!\n");
+    printf(GRN "========> " CYN "Cloning fakeos and preparing for build. Please make sure your USB is connected." YEL "\n");
+    int cloneCommand = system("sudo git clone https://github.com/reversepwn/fake-os /chrome-pie/temp/fakeos/");
+    if(cloneCommand == 0) {
+        printf(GRN "Successfully cloned fakeos\n");
+        printf(YEL "Will now continue to build fakeos, dont plug out your USB\n" CRESET);
+    } else {
+        printf(RED "Could not clone fakeos, exited with error\n");
+        return 1;
+    }
     return 0;
 }
